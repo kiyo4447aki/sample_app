08d5d6d18df6e992d4c7ed0959b6bdc78fbc4d47
kiyo4447aki <kiyo4447aki@gmail.com>
2024-07-22 04:12:39 +0900

    commit

--- src/routes/Top/Top.test.js
+++ src/routes/Top/Top.test.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import Top from "./Top"
-import { BrowserRouter } from "react-router-dom"
+import Game from "../Game/Game"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import userEvent from "@testing-library/user-event"
 
 describe("Top", () => {
 	test("Topコンポーネントのレンダリング", () => {
@@ -10,5 +12,20 @@ describe("Top", () => {
 		expect(title).toHaveLength(2)
 		expect(screen.getByText("数字・記号専用のタイピング練習ゲーム")).toBeInTheDocument()
 		expect(screen.getByRole("button")).toBeInTheDocument()
+	})
+	test("ボタンを押下し、プレイページに遷移できる", async () => {
+		render(
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" Component={Top} />
+					<Route path="/play" Component={Game} />
+				</Routes>
+			</BrowserRouter>
+		)
+		const user = userEvent.setup()
+		await user.click(screen.getByRole("button"))
+		expect(
+			screen.getByText("表示された数字または記号のキーを押してください")
+		).toBeInTheDocument()
 	})
 })
