a7d2d3ad83bfa8e5be413de1a8e5a0ab7d5187fe
kiyoaki <kiyo4447aki@gmail.com>
2024-07-23 03:17:53 +0900

    commit

--- src/routes/Result/Result.test.js
+++ src/routes/Result/Result.test.js
@@ -21,5 +21,30 @@ describe("Result", () => {
 		})
 		const { container } = render(<Result />, { wrapper: BrowserRouter })
 		expect(screen.getByText("結果")).toBeInTheDocument()
+		expect(screen.getByText("経過時間: ")).toBeInTheDocument()
+		expect(screen.getByText("正しく打ったキーの数")).toBeInTheDocument()
+		expect(screen.getByText("平均キータイプ数")).toBeInTheDocument()
+		expect(screen.getByText("ミスタイプ数")).toBeInTheDocument()
+		expect(screen.getByText("正確率")).toBeInTheDocument()
+	})
+	test("タイトルに戻るボタンを押下し、トップページに遷移できる", async () => {
+		const mockUseLocation = useLocation
+		mockUseLocation.mockReturnValue({
+			state: {
+				missCount: 5,
+				time: 100000,
+			},
+		})
+		render(
+			<MemoryRouter initialEntries={["/result"]}>
+				<Routes>
+					<Route path="/result" Component={Result} />
+					<Route path="/" Component={Top} />
+				</Routes>
+			</MemoryRouter>
+		)
+		const user = userEvent.setup()
+		await user.click(screen.getByRole("button"))
+		expect(screen.getByText("数字・記号専用のタイピング練習ゲーム")).toBeInTheDocument()
 	})
 })
